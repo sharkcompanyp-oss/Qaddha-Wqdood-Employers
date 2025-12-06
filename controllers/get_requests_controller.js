8a0854d7fa3c7541f7d62ea486913f8fdc3a0963
Rasheed963 <rasheedhaddad2018@gmail.com>
2025-12-06 09:43:55 +0300

    Nothing

--- controllers/get_requests_controller.js
+++ controllers/get_requests_controller.js
@@ -2,7 +2,10 @@ import request from "../models/request";
 export const Get_Requests = async (req, res) => {
   try {
     const Requests = await request.find();
-    res.status(200).json(Requests);
+    if (!Requests || Requests.length == 0) {
+      return res.status(404).json({ message: "لا توجد أي طلبات معلقة حاليا" });
+    }
+    return res.status(200).json(Requests);
   } catch (err) {
     res.status(500).json("حدث خطأ في الخادم.");
   }
